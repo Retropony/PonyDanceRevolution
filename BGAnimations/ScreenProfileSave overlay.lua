886533bce712635062c16cf1a409a5f7183f32d5
Retropony <retropone@derpymail.org>
2016-07-20 14:43:13 +0200

    Change Cutie Mark link

--- BGAnimations/ScreenProfileSave overlay.lua
+++ BGAnimations/ScreenProfileSave overlay.lua
@@ -1,5 +1,5 @@
 local x = Def.ActorFrame{
-	LoadActor(THEME:GetPathG("", "/ScreenWithMenuElements header/timer"))..{
+	LoadActor(THEME:GetPathG("", "_cm"))..{
 		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-25;);
 		OnCommand=cmd(diffusealpha,0;smooth,0.2;diffusealpha,0.8;sleep,0.4);
 		OffCommand=cmd(smooth,0.3;diffusealpha,0;);
